fd9ffc6d1e9c31c8dd191a77ff766dddfb8400b7
odinellefsen <odin@ellefsen.fo>
2024-04-29 14:14:09 +0100

    made awesome map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct Person {
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let people = vec![
+        Person {
+            name: "Odin".to_string(),
+        },
+        Person {
+            name: "Baldur".to_string(),
+        },
+    ];
+
+    let names: Vec<String> = people.iter().map(|p| p.name.clone()).collect();
+    println!("{:?}", names);
 }
